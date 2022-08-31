e75c949c56192dcd6c3f2b3fbd9a2f9388a779a4
Gosha Kokorin <go_2002@mail.ru>
2022-08-31 10:18:58 +0500

    add post request

--- app/api/v1/endpoints/files.py
+++ app/api/v1/endpoints/files.py
@@ -1,37 +1,51 @@
 from fastapi import APIRouter
 from fastapi.responses import FileResponse
 from docxtpl import DocxTemplate
+from app.schemas.user import ChangeUser
 
 router = APIRouter(prefix='/files', tags=['Files'])
 
 
 @router.get('/{filename}')
-def files(filename,
-          name: str = '',
-          surname: str = '',
-          patronymic: str = '',
-          phone: str = '',
-          passport: str = '',
-          seria: str = '',
-          nomer: str = '',
-          address: str = '',
-          familyComposition: str = '',
-          birthday: str = '',
-          gender: str = ''):
-    context = {'name': name,
-               'surname': surname,
-               'patronymic': patronymic,
-               'phone': phone,
-               'passport': passport,
-               'seria': seria,
-               'nomer': nomer,
-               'address': address,
-               'familyComposition': familyComposition,
-               'birthday': birthday,
-               'gender': gender}
+def files(filename):
+    context = {'name': '',
+               'surname': '',
+               'patronymic': '',
+               'phone': '',
+               'passport': '',
+               'seria': '',
+               'nomer': '',
+               'address': '',
+               'familyComposition': '',
+               'birthday': '',
+               'gender': ''}
     doc = DocxTemplate('app/files/templates/' + filename + '.docx')
     doc.render(context)
     doc.save('app/files/generated/' + filename + '.docx')
 
     return FileResponse('app/files/generated/' + filename + '.docx',
                         media_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document')
+
+
+@router.post('/{filename}')
+def files(filename, user: ChangeUser):
+    # seria =
+    # nomer =
+    context = {'name': user.name,
+               'surname': user.surname,
+               'patronymic': user.patronymic,
+               'phone': user.phone,
+               'passport': user.passport,
+               'seria': '',
+               'nomer': '',
+               'address': user.address,
+               'familyComposition': user.familyComposition,
+               'birthday': user.birthday,
+               'gender': user.gender}
+    doc = DocxTemplate('app/files/templates/' + filename + '.docx')
+    doc.render(context)
+    doc.save('app/files/generated/' + filename + '.docx')
+
+    return FileResponse('app/files/generated/' + filename + '.docx',
+                        media_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                        filename=filename + 'docx')
