ec2c0445e3a37e877b259165266275e575e26ae3
Gosha Kokorin <go_2002@mail.ru>
2022-08-31 16:09:55 +0500

    fix validate 3

--- app/api/v1/endpoints/files.py
+++ app/api/v1/endpoints/files.py
@@ -1,3 +1,4 @@
+import datetime
 from fastapi import APIRouter
 from fastapi.responses import FileResponse
 from docxtpl import DocxTemplate
@@ -45,7 +46,7 @@ def files(filename, user: ChangeUser):
     else:
         gender = ''
 
-    if user.birthday == '3000-01-01':
+    if user.birthday == datetime.date(3000, 1, 1):
         birthday = ''
     else:
         birthday = user.birthday
@@ -62,6 +63,54 @@ def files(filename, user: ChangeUser):
                'birthday': birthday,
                'gender': gender}
 
+    print(user.birthday)
+    print(birthday)
+    print(context)
+
+    doc = DocxTemplate('app/files/templates/' + filename + '.docx')
+    doc.render(context)
+    doc.save('app/files/generated/' + filename + '.docx')
+
+    return FileResponse('app/files/generated/' + filename + '.docx',
+                        media_type='application/vnd.openxmlformats-officedocument.wordprocessingml.document',
+                        filename=filename + '.docx')
+
+
+@router.put('/{filename}')
+def files(filename, user: ChangeUser):
+    if user.passport is not None:
+        seria = user.passport[:4]
+        nomer = user.passport[5:]
+    else:
+        seria = ''
+        nomer = ''
+
+    if user.gender == 'male':
+        gender = 'Мужчина'
+    elif user.gender == 'female':
+        gender = 'Девушка'
+    else:
+        gender = ''
+
+    if user.birthday == datetime.date(3000, 1, 1):
+        birthday = ''
+    else:
+        birthday = user.birthday
+
+    context = {'name': user.name,
+               'surname': user.surname,
+               'patronymic': user.patronymic,
+               'phone': user.phone,
+               'passport': user.passport,
+               'seria': seria,
+               'nomer': nomer,
+               'address': user.address,
+               'familyComposition': user.familyComposition,
+               'birthday': birthday,
+               'gender': gender}
+
+    print(user.birthday)
+    print(birthday)
     print(context)
 
     doc = DocxTemplate('app/files/templates/' + filename + '.docx')
